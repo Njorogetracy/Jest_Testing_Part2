9aefe4bdeb09ceea00769a155aff55eac060f01f
Hope Tracy Njoroge <108297287+Njorogetracy@users.noreply.github.com>
2023-05-10 16:21:27 +0200

    add game score tests

--- 00_boilerplate/scripts/tests/game.test.js
+++ 00_boilerplate/scripts/tests/game.test.js
@@ -1,8 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const { game } = require("../game")
 
 beforeAll(() => {
     let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
+    let fileContents = fs.readFileSync("../index.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
@@ -12,4 +16,14 @@ describe("game object contains correct keys", () => {
     test("score key exits", () => {
         expect("score" in game).toBe(true);
     });
+    test("CurrentGame key exits", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exits", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exits", () => {
+        expect("choices" in game).toBe(true);
+    });
 }) ;
+
